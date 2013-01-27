e4524358e23e85a055d48d0315246d9918149369
Nicholas Bergson-Shilcock <me@nicholasbs.net>
2013-01-27 14:43:56 -0500

    Simple DSL for dog animations

--- javascript/main.js
+++ javascript/main.js
@@ -1,20 +1,40 @@
 $(document).ready(function () {
-  //setTimeout(function() {$('#birthday-video').fadeIn();}, 2000); 
-  var $dog = $('#dog'),
-      xs = [-$dog.width(), // offscreen left
-            $(document).width()/2 - $dog.width()/2 - 50, // ~ centered
+  var dogWidth = $('#dog').width(),
+      xs = [-dogWidth, // offscreen left
+            $(document).width()/2 - dogWidth/2 - 50, // ~ centered
             $(document).width()], // offscreen right
-      frames = ['images/dog1.png', 'images/dog2.png', 'images/dog3.png'],
-      i = 0,
-      left,
-      timer = setInterval(function () {
-          left = $dog.offset().left + 15;
-          $dog.attr('src', frames[++i % 2]).offset({left: left});
-          if (left > xs[1]) {
-            clearInterval(timer);
-            $dog.attr('src', frames[2]);
-          }
-        }, 250);
+      frames = ['images/dog1.png', 'images/dog2.png', 'images/dog3.png'];
 
-  $dog.offset({top: 100, left: xs[0]});
+  // Yes, I wrote a DSL for this.
+  start({'at': [100, xs[0]], 'then': function () {
+    walk({'to': xs[1], 'then': function () {
+      sit({'for': 3000, 'then': function() {
+        walk({'to': xs[2]});
+      }});
+    }});
+  }});
+
+  function start (o) {
+    $('#dog').offset({top: o.at[0], left: o.at[1]});
+    o.then();
+  }
+
+  function walk (o) {
+    var i = 0, left, timer;
+    timer = setInterval(function () {
+      left = $('#dog').offset().left + 15;
+      $('#dog').attr('src', frames[++i % 2]).offset({left: left});
+      if (left > o.to) {
+        clearInterval(timer);
+        if (o.then) o.then();
+      }
+    }, 250);
+  }
+
+  function sit (o) {
+    $('#dog').attr('src', frames[2]);
+    setTimeout(function () {
+      o.then();
+    }, o['for']);
+  }
 });
